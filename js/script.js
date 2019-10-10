f7fb7a318efd8e287b4cbaa664b63e248377ddf6
kl-marvin <kl.marvin@outlook.com>
2019-10-10 15:59:58 +0200

    JS - adding logic on images

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// create varibles to catch images with ids 
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
+
+// create variable as path 
+let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
+
