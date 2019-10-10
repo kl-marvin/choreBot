ccdc592488ed285a03a861e80eb845fc9db7677a
kl-marvin <kl.marvin@outlook.com>
2019-10-10 16:07:49 +0200

    JS - on click function added to change src path

--- js/script.js
+++ js/script.js
@@ -9,3 +9,45 @@ let botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-do
 let beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 let spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
 
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+
+const randomChoreDoorGenerator = () => {
+  // create a value between 0 and 2 that will change the path of the images 
+choreDoor = Math.floor(Math.random() * numClosedDoors);
+  
+  if(choreDoor === 0){
+    openDoor1 = botDoorPath;
+    openDoor2 =  beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  }else if(choreDoor === 1){
+   openDoor2 = botDoorPath;
+   openDoor1 = beachDoorPath;
+   openDoor3 = spaceDoorPath;
+  }else { (choreDoor === 2)
+    openDoor3 = botDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor1 = beachDoorPath;
+  }
+}
+
+// on click generate a new path for the image
+doorImage1.onclick = () => {
+    // change src of doorImage1 on click 
+    doorImage1.src = openDoor1;
+  }
+  
+  doorImage2.onclick = () => {
+    // change src of doorImage2 on click 
+    doorImage2.src = openDoor2;
+  }
+  
+  doorImage3.onclick = () => {
+    // change src of doorImage3 on click 
+    doorImage3.src = openDoor3;
+  }
+  
+  
